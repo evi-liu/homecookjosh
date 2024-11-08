9317d867d39570855767ac881410b858f071da43
evi-liu <bbmayg@gmail.com>
2024-11-08 15:23:06 -0800

    added scallion pancakes and pork rib soup

--- js/recipes.js
+++ js/recipes.js
@@ -1,12 +1,5 @@
 const CHINESE_RECIPES = [
     {
-        title: "Char Siu (Chinese BBQ Pork)",
-        image: "./images/char-siu.jpg",
-        url: "./recipes/char-siu.html",
-        time: "2 hours | Overnight",
-        type: "Chinese"
-    },
-    {
         title: "Hand-Pulled Noodles",
         image: "./images/hand-pulled-noodles.jpg",
         url: "./recipes/hand-pulled-noodles.html",
@@ -15,44 +8,23 @@ const CHINESE_RECIPES = [
     },
     {
         title: "Scallion Pancakes",
-        image: "https://images.unsplash.com/photo-1590929713992-005e2eeea04a",
+        image: "./images/scallion-pancakes.jpg",
         url: "./recipes/scallion-pancakes.html",
         time: "45 minutes",
         type: "Chinese"
     },
     {
-        title: "Mapo Tofu",
-        image: "https://images.unsplash.com/photo-1617093727343-374698b1b08d",
-        url: "./recipes/mapo-tofu.html",
-        time: "30 minutes",
+        title: "Taiwanese Pork Rib Soup",
+        image: "./images/pork-rib-soup.jpg",
+        url: "./recipes/pork-rib-soup.html",
+        time: "50 minutes",
         type: "Chinese"
     },
     {
-        title: "Dan Dan Noodles",
-        image: "https://images.unsplash.com/photo-1617093727343-374698b1b08d",
-        url: "./recipes/dan-dan-noodles.html",
-        time: "40 minutes",
-        type: "Chinese"
-    },
-    {
-        title: "Braised Pork Belly (Hong Shao Rou)",
+        title: "Char Siu (Chinese BBQ Pork)",
         image: "./images/char-siu.jpg",
-        url: "./recipes/braised-pork-belly.html",
-        time: "2 hours",
-        type: "Chinese"
-    },
-    {
-        title: "Sichuan Dry Fried Green Beans",
-        image: "https://images.unsplash.com/photo-1625944525533-473f1a3d54e7",
-        url: "./recipes/sichuan-dry-fried-green-beans.html",
-        time: "30 minutes",
-        type: "Chinese"
-    },
-    {
-        title: "Congee (Rice Porridge)",
-        image: "https://images.unsplash.com/photo-1626509653291-c51a8ee8c4a9",
-        url: "./recipes/congee.html",
-        time: "30 minutes",
+        url: "./recipes/char-siu.html",
+        time: "2 hours | Overnight",
         type: "Chinese"
     }
 ];
